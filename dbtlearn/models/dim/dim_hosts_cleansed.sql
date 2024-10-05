a9718aec31fc705dbca1fa17a04cc95624a506c8
techksraj <tech.ksraj541@gmail.com>
2024-10-05 20:34:43 +0000

    Added Materializations

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,11 @@
+WITH src_hosts as (
+    select * from {{ ref('src_hosts') }}
+)
+select
+    host_id,
+    NVL(host_name,'Anonymous') AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM src_hosts
+
